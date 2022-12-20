9bbfedd7b66ea674ee5a10ab5ba63fbe76afb56c
Silpashree <silpashree@bootlabstech.com>
2022-12-20 15:16:11 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "acr_name" {
+  type        = string
+  description = "name of the acr"
+}
+variable "resource_group_name" {
+  type        = string
+  description = "rg of the acr"
+}
+variable "location" {
+  type        = string
+  description = "location of the acr"
+}
+variable "acr_sku" {
+  type        = string
+  description = "sku for the acr"
+}
+variable "admin_enabled" {
+  type        = bool
+  description = "whether admin is enabled for the acr"
+}
+variable "public_network_access_enabled" {
+  type        = bool
+  description = "whether public_network_access_enabled for the acr"
+}
+variable "export_policy_enabled" {
+  type        = bool
+  description = "whether exportpolicy is enabled for the acr"
+}
